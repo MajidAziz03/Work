ac6e495307b3a355c2eee03120a533b106ecd931
Majid Aziz <majidaziz7ctech@gmail.com>
2023-04-12 15:42:14 +0500

    fix closed mmodel

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const modal = document.querySelector(".modal-active")
 const btn = document.querySelector('.btnView')
 const modalBackdrop = document.querySelector('.modal')
 const crossed = document.querySelector('.cross')
+
+
 const cat1 = document.querySelector('.card-cat.cat-1')
 const cat2 = document.querySelector('.card-cat.cat-2')
 const cat3 = document.querySelector('.card-cat.cat-3')
@@ -11,6 +13,11 @@ const modalCat1 = document.querySelector(".modal-category1")
 const modalCat2 = document.querySelector(".modal-category2")
 const modalCat3 = document.querySelector(".modal-category3")
 const modalCat4 = document.querySelector(".modal-category4")
+
+const crossCat1 = document.querySelector(".cross-cat1")
+const crossCat2 = document.querySelector(".cross-cat2")
+const crossCat3 = document.querySelector(".cross-cat3")
+const crossCat4 = document.querySelector(".cross-cat4")
 
 
 
@@ -36,4 +43,25 @@ cat3.addEventListener('click', () => {
 
 cat4.addEventListener('click', () => {
     modalCat4.classList.remove("modal-category-active4")
+})
+
+
+
+modalCat1.addEventListener("click", () => {
+    modalCat1.classList.add("modal-category-active1")
+})
+
+
+modalCat2.addEventListener("click", () => {
+    modalCat2.classList.add("modal-category-active2")
+})
+
+
+modalCat3.addEventListener("click", () => {
+    modalCat3.classList.add("modal-category-active3")
+})
+
+
+modalCat4.addEventListener("click", () => {
+    modalCat4.classList.add("modal-category-active4")
 })
